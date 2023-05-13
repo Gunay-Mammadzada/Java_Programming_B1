970f9fd041ad0e69188d9b6340750557ed819e9b
Gunay-Mammadzada <marvelus83@gmail.com>
2023-05-13 13:59:49 -0400

    day_25_work_method_practice

--- src/day25_methods/FrequencyOfCharacter.java
+++ src/day25_methods/FrequencyOfCharacter.java
@@ -1,18 +1,21 @@
 package day25_methods;
 
 public class FrequencyOfCharacter {
-    public static int frequencyOfCharacter(String str , char ch){
 
-        int count = 0;
+        public static int frequencyOfCharacter(String str , char ch){
 
-        for (int i = 0; i <str.length() ; i++) {
-            if (str.charAt(i)==ch){
-                count++;
+            int count = 0;
 
+            for (int i = 0; i <str.length() ; i++) {
+                if (str.charAt(i)==ch){
+                    count++;
+
+
+                }
 
             }
-
+            return count;
         }
-        return count;
     }
-}
+
+
