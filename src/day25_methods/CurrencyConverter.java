f2df2129b6e856da6e8090f9362434ca6e6d2895
Gunay-Mammadzada <marvelus83@gmail.com>
2023-05-16 23:15:40 -0400

    day_25_work_method_practice

--- src/day25_methods/CurrencyConverter.java
+++ src/day25_methods/CurrencyConverter.java
@@ -1,38 +1,7 @@
 package day25_methods;
 
 
-public class CurrencyConverter {
-    public static double currency (String currencyType, double currencyAmount){
 
-        switch (currencyType){
-            case "euro":
-                return  currencyAmount * 0.91;
-
-            case "yen":
-                return currencyAmount *121.03;
-            case "lira":
-                return currencyAmount * 14.85;
-            case "won":
-                return currencyAmount* 1_217.52;
-            case "rupee":
-                return currencyAmount * 181.45;
-            default:
-                System.out.println("Not valid currency");
-                return 0.0;
-
-
-
-
-        }
-    }
-
-    public static void main(String[] args) {
-        System.out.println("You converted currency: " +currency("Euro" ,500));
-        System.out.println("You converted currency: " +currency("YEN" ,500));
-    }
-
-
-}
 
 /*
 create a method that will accept a currency type(String) and a currency amount (double)[as dollars] and convert the currency from dollars to a different type
