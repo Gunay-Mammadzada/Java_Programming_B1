efaade1c5a2d6f20bc9aaa52e4a5109c5f252999
Gunay-Mammadzada <marvelus83@gmail.com>
2023-07-12 13:16:31 -0400

    day_34

---
+++ src/day34_b_encapsulation/static_import/WithWilder.java
@@ -0,0 +1,8 @@
+package day34_b_encapsulation.static_import;
+import static java.util.Arrays.*;
+public class WithWilder {
+    public static void main(String[] args) {
+        int [] a = {1,2,3,4,5};
+        sort(a);
+    }
+}
